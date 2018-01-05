7ae1a24af3133ec061d10499c5e0da7141b39541
joannamoskal <asiadol@gmail.com>
2018-01-05 19:35:23 +0100

    lesson 1 : hello world

---
+++ javacourse/hello world/src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Tim!");
+
+        int MyFirstNumber = (10 + 5) + (2*10);
+        int MySecondNumber  = 12;
+        int MyThird Number =
+
+        System.out.println(MyFirstNumber);
+        System.out.println("MySecondNumber");
+    }
+}
