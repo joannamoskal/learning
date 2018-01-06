10c98b1e9fe6e7c7ac7ded08c5a61524df2d7628
joannamoskal <asiadol@gmail.com>
2018-01-06 20:45:14 +0100

    lesson 5: char

---
+++ javacourse/charandboolean/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        // 1. Find the code for the registered symbol on the same line as the copyright symbol.
+        // 2. Create a variable of type char and assign it the unicode value for that symbol.
+        // 3. Display in on screen.
+
+        //00AE
+        char registeredValue = '\u00AE';
+        System.out.println("registeredValue = " + registeredValue);
+
+    }
+}
