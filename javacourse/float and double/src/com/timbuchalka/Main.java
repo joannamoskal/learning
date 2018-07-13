02891661ce0c7de5921a4d5192fbae2c14321bb3
joannamoskal <asiadol@gmail.com>
2018-07-13 08:35:45 +0200

    float and double

---
+++ javacourse/float and double/src/com/timbuchalka/Main.java
@@ -0,0 +1,11 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myIntValue = 5;
+        float myFloatValue = 5f * 0.45359237f;
+        System.out.println("myIntValue =" + myIntValue);
+        System.out.println("myFloatValue =" + myFloatValue);}
+    }
+}
