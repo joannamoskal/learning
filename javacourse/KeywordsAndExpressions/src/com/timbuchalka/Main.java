8cf4e57258b94cd9b0c5e494d27337441cf0e063
joannamoskal <asiadol@gmail.com>
2018-08-08 19:15:34 +0200

    keywords and expressions

---
+++ javacourse/KeywordsAndExpressions/src/com/timbuchalka/Main.java
@@ -0,0 +1,10 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args)
+    int score =100;
+    if(score > 99){
+        System.out.println("You got the high score");
+                  }
+}
