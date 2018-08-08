c222744712b62fb6968931ab6747118b8205bd4f
joannamoskal <asiadol@gmail.com>
2018-08-08 19:16:24 +0200

    statements white space indenting

---
+++ javacourse/StatementsWhiteSpaceAndIndenting/src/com/timbuchalka/Main.java
@@ -0,0 +1,17 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVariable=50;
+        //statements
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+        System.out.println("This is" +
+                " another " +
+                " still more.");
+        // write your code here
+    }
+}
+
