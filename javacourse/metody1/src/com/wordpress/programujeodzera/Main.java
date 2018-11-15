555e7c054a27927dd3a57e07b32157f01091c699
joannamoskal <asiadol@gmail.com>
2018-11-15 14:59:12 +0100

    methods cd

--- javacourse/metody1/src/com/wordpress/programujeodzera/Main.java
+++ javacourse/metody1/src/com/wordpress/programujeodzera/Main.java
@@ -9,6 +9,12 @@ public class Main {
         calculateScore(75);
         calculateScore();
 
+        double centimetres = calcFeetAndInchesToCentimeters(-10, 0);
+        if (centimetres < 0.0) {
+            System.out.println("Invalid parameters");
+
+        }
+        calcFeetAndInchesToCentimeters(100);
     }
 
     public static int calculateScore(String playerName, int score) {
@@ -30,4 +36,41 @@ public class Main {
         return 0;
 
     }
+
+    public static double calcFeetAndInchesToCentimeters
+            (double feet, double inches) {
+        if ((feet < 0) || ((inches < 0) && (inches > 12))) {
+            System.out.println("Invalid feet and inches");
+            return -1;
+
+        }
+        double centimeters = (feet) * 12 * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println(feet + " feet, " + inches + " inches =" + centimeters + "cm");
+        return centimeters;
+
+    }
+
+    public static double calcFeetAndInchesToCentimeters
+            (double inches) {
+        if ((inches < 0)) {
+            return -1;
+        }
+
+        double feet = (int) inches / 12;
+        double remainingInches = (int) inches % 12;
+        System.out.println(inches + " inches is equal to " + feet +
+                " feet and " + remainingInches + "inches");
+        return calcFeetAndInchesToCentimeters(feet, remainingInches);
+
+
+    }
 }
+
+
+
+
+
+
+
+
