d3d1704b28e0d8718b7830ddcf55fbc2d2ed57b2
joannamoskal <asiadol@gmail.com>
2018-11-15 15:00:18 +0100

    switch statement

---
+++ javacourse/switchStatement/src/com/wordpress/programujeodzera/Main.java
@@ -0,0 +1,54 @@
+package com.wordpress.programujeodzera;
+
+public class Main {
+    public static void main ( String [] args){
+//    int value = 1;
+//    if (value == 1){
+//        System.out.println("Value was 1");}
+//    else if (value == 2){
+//        System.out.println("Value was 2");
+//    }else {
+//        System.out.println("Value was not 1 or 2");
+//    }
+//    int switchValue = 3;
+//    switch (switchValue){
+//        case 1:
+//            System.out.println("Value was 1");
+//            break;
+//        case 2:
+//            System.out.println("Value was 2");
+//            break;
+//        case 3: case 4: case 5:
+//            System.out.println("was a 3, or a 4, or a 5");
+//            System.out.println("Actually was a " + switchValue);
+//            break;
+//        default:
+//            System.out.println("Was not 1 or 2");
+//            break;
+//
+//    }
+     char switchValue1 = 'A';
+    switch (switchValue1){
+        case 'A':
+            System.out.println("A was found");
+            break;
+        case 'B':
+            System.out.println("B was found");
+            break;
+        case 'C':
+            System.out.println("C was found");
+            break;
+        case 'D':
+            System.out.println("D was found");
+            break;
+        case 'E':
+            System.out.println("E was found");
+            break;
+        default:
+            System.out.println("A,B,C,D or E was not found");
+            break;
+
+    }
+    }
+}
+
