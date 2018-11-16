e8a1c39473c0b128b748fa71415704510e5b6175
joannamoskal <asiadol@gmail.com>
2018-11-16 16:48:11 +0100

    piramidy

---
+++ javacourse/piramidy/src/com/wordpress/programujeodzera/Main.java
@@ -0,0 +1,14 @@
+package com.wordpress.programujeodzera;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        for (int i =1; i<2 ; i++) {
+            System.out.println(" " + " " + i + " ");
+            System.out.println(" " + i + "" + i + "" + i + " ");
+            System.out.println(i + "" + i + "" + i + "" + i + "" + i);
+        }
+    }
+}
