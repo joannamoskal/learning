5feb81ec52c59d370ceecbe3d75ed95770ed6377
joannamoskal <asiadol@gmail.com>
2019-06-12 22:07:52 +0200

    for statement, piramidy, switch statement, while and do while statements

---
+++ javacourse/for statement1/src/com/wordpress/programujeodzera/Main.java
@@ -0,0 +1,9 @@
+package com.wordpress.programujeodzera;
+
+public class Main {
+   public static void main (String[]args){
+       int count = 0;
+       for ( i=10, i<50, i++){
+
+       }}
+}
--- javacourse/forStatement/src/com/wordpress/programujeodzera/Main.java
+++ javacourse/forStatement/src/com/wordpress/programujeodzera/Main.java
@@ -14,9 +14,28 @@ public class Main {
         for(int i=8; i<=8&&i>=2; i--){
             System.out.println("10,000 at " + i+"%" + " = " + String.format("%.2f",calculateInterest(10000.0,i)));
         }
+        int count = 0;
+        for ( int i=10, i<50, i++){
+
+        }
 
     }
+
+
+    public static boolean isPrime (int n){
+        if (n==1){
+            return false;
+        }
+        for (int i=2, i<= n/2; i++){
+            if (n% i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static double calculateInterest(double amount, double interestRate){
         return(amount*(interestRate/100));
     }
-}
+    }
+
