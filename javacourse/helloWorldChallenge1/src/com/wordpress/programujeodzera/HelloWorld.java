669a4c642ea711430f54588f440ef762adc54007
joannamoskal <asiadol@gmail.com>
2019-06-19 21:01:53 +0200

    8_kyu: Function 1 - hello world.

---
+++ javacourse/helloWorldChallenge1/src/com/wordpress/programujeodzera/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.wordpress.programujeodzera;
+//Make a simple function called greet that returns the most-famous "hello world!".
+
+public class HelloWorld {
+    public static String greet(){
+        return "hello world!";
+    }
+}
