945a8f0b17ea6a7e29a594f021d2670b851aa148
joannamoskal <asiadol@gmail.com>
2019-06-20 13:08:09 +0200

    konstruktory

--- javacourse/constructors/src/com/wordpress/programujeodzera/Account.java
+++ javacourse/constructors/src/com/wordpress/programujeodzera/Account.java
@@ -19,7 +19,46 @@ public class Account {
             this.balance -= withdrawalAmount;
             System.out.println("Withdrawal of " + withdrawalAmount + "processed. Remaining balance = " + this.balance);
 
+        }}
+
+        public String getNumber() {
+            return number;
         }
 
+        public void setNumber(String number){
+            this.number = number;
+        }
 
-}}
+        public double getBalance() {
+            return balance;
+        }
+
+        public void setBalance(double balance) {
+            this.balance = balance;
+        }
+
+        public String getCustomerName() {
+            return customerName;
+        }
+
+        public void setCustomerName(String customerName) {
+            this.customerName = customerName;
+        }
+
+        public String getCustomerEmailAddress() {
+            return customerEmailAddress;
+        }
+
+        public void setCustomerEmailAddress(String customerEmailAddress) {
+            this.customerEmailAddress = customerEmailAddress;
+        }
+
+        public String getCustomerPhoneNumber() {
+            return customerPhoneNumber;
+        }
+
+        public void setCustomerPhoneNumber(String customerPhoneNumber) {
+            this.customerPhoneNumber = customerPhoneNumber;
+        }
+    }
+
--- javacourse/constructors/src/com/wordpress/programujeodzera/Main.java
+++ javacourse/constructors/src/com/wordpress/programujeodzera/Main.java
@@ -4,7 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         Account bobsAccount =  new Account();
+        bobsAccount.setNumber("12345");
+        bobsAccount.setBalance(0.00);
+        bobsAccount.setCustomerName("Bob Brown");
+        bobsAccount.setCustomerEmailAddress("myemail@bob.com");
+        bobsAccount.setCustomerPhoneNumber("(54) 233 23 23)");
         bobsAccount.withdrawal(100.0);
+
 
         bobsAccount.deposit(50.0);
         bobsAccount.withdrawal(100.0);
